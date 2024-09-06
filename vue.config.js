2ca7453ba5d088513b179ca99cc1f6686a84b529
吴金云 <wujinyun@cleartv.cn>
2024-09-06 09:55:11 +0800

    fix: 修改基础地址配置

--- vue.config.js
+++ vue.config.js
@@ -26,9 +26,9 @@ module.exports = {
    * In most cases please use '/' !!!
    * Detail: https://cli.vuejs.org/config/#publicpath
    */
-  publicPath: '/',
+  publicPath: './',
   outputDir: 'web',
-  assetsDir: 'static/web',
+  assetsDir: './static/web',
   lintOnSave: false, // process.env.NODE_ENV === 'development',
   productionSourceMap: false,
   devServer: {
